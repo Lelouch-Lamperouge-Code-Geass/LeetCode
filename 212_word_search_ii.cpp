77ce625b2bbc849fa90443535f2c88d6650a6da3
Augustus Pope <yl916@nyu.edu>
2016-12-21 16:13:53 -0500

    DNA sequecnes

--- 212_word_search_ii.cpp
+++ 212_word_search_ii.cpp
@@ -1,67 +1,76 @@
-class TrieNode {
-public:
-  TrieNode():m_end_here(0),m_children(26,nullptr) {}
-  ~TrieNode() { for (TrieNode* child : m_children) delete child;}
-  int m_end_here;
-  vector<TrieNode*> m_children;
-};
-
-class Trie {
-public:
-  Trie():m_root(new TrieNode()){}
-  ~Trie() { delete m_root; }
-  void AddWord(const string & word) {
-    TrieNode* curr(m_root);
-    for (char c : word) {
-      int i = c - 'a';
-      if (!curr->m_children[i]) curr->m_children[i] = new TrieNode();
-      curr = curr->m_children[i];
-    }
-    ++ curr->m_end_here;
-  }
-  TrieNode* m_root;
-};
-
 class Solution {
 public:
+  class TrieNode {
+  public:
+    TrieNode() : m_count(0),m_children(26,nullptr) {}
+    ~TrieNode() {for (TrieNode* child : m_children) delete child;}
+    int m_count;
+    vector<TrieNode*> m_children;
+  };
+
+  class Trie {
+  public:
+    Trie() : m_root(new TrieNode()) { }
+    ~Trie() {delete m_root;}
+    void AddWord(const std::string & word) {
+      TrieNode* curr(m_root);
+      for (const char c : word) {
+        if (!curr->m_children[c-'a']) {
+          curr->m_children[c-'a'] = new TrieNode();
+        }
+        curr = curr->m_children[c-'a'];
+      }
+      ++ curr->m_count;
+    }
+    TrieNode* m_root;
+  };
+
   vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
-    if (board.empty()) return vector<string>();
+    if(board.empty() || words.empty()) return vector<string>();
     Trie trie;
-    for (const string & word : words) {
+    for (const std::string & word : words) {
       trie.AddWord(word);
     }
-    vector<string> reval;
-    string temp;
-    const int m(board.size()),n(board[0].size());
-    for (int i=0;i<m;++i) {
-      for (int j=0;j<n;++j) {
-        find(board,i,j,trie.m_root,reval,temp);
+
+    vector<string> res;
+    const std::size_t row_size(board.size()),col_size(board[0].size());
+    std::string temp("");
+    for (std::size_t i=0;i<row_size;++i) {
+      for (std::size_t j=0;j<col_size;++j) {
+        FindWithDfs(res,temp,board,trie.m_root,i,j);
       }
     }
-    return reval;
+    return res;
   }
 private:
-  void find(vector< vector<char> > & board,
-            int i,
-            int j,
-            TrieNode* node,
-            vector<string> & reval,
-            string & temp) {
-    const int row_size(board.size()), col_size(board[0].size());
-    if (i<0 || i>=row_size || j<0 || j>=col_size) return;
-    if (!node) return;
-    if (node->m_end_here > 0) {
-      reval.push_back(temp);
+  void FindWithDfs(vector<string> & res,
+                   string & temp,
+                   vector<vector<char>>& board,
+                   const TrieNode * node_ptr,
+                   const std::size_t row,
+                   const std::size_t col) {
+    const std::size_t row_size(board.size()),col_size(board[0].size());
+    if (row>=row_size || col>=col_size || board[row][col] == '#' || !node_ptr)
+      return; // notice we are using size_t which is non-negative
+    const char cur_char = board[row][col];
+    if (!node_ptr->m_children[cur_char-'a'])
+      return;
+    board[row][col] = '#';
+    temp.push_back(cur_char);
+    if (node_ptr->m_children[cur_char-'a']->m_count>0) { // find a word
+      res.push_back(temp);
+      // We can use a HashSet to de-duplicate, but here we reset m_count
+      // in trie. It is a small trick
+      node_ptr->m_children[cur_char-'a']->m_count = 0;
     }
-    node = node->m_children[board[i][j] - 'a'];
-    const char original_char = board[i][j];
-    board[i][j] ='#';
-    temp.push_back(board[i][j]);
-    find(board,i+1,j,node,reval,temp);
-    find(board,i-1,j,node,reval,temp);
-    find(board,i,j+1,node,reval,temp);
-    find(board,i,j-1,node,reval,temp);
-    temp.pop_back(); // revert back
-    board[i][j] = original_char;
+    node_ptr = node_ptr->m_children[cur_char-'a'];
+    FindWithDfs(res,temp,board,node_ptr,row-1,col);
+    FindWithDfs(res,temp,board,node_ptr,row+1,col);
+    FindWithDfs(res,temp,board,node_ptr,row,col-1);
+    FindWithDfs(res,temp,board,node_ptr,row,col+1);
+
+    //revert back
+    temp.pop_back();
+    board[row][col] = cur_char;
   }
 };
