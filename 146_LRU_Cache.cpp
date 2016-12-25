2378c9923655f75d33a8717dceb992a8250c5d33
Augustus Pope <yl916@nyu.edu>
2016-12-25 16:17:00 -0500

    remove duplicate letters

--- 146_LRU_Cache.cpp
+++ 146_LRU_Cache.cpp
@@ -1,37 +1,48 @@
 class LRUCache{
 public:
-  LRUCache(int capacity) : m_capacity(capacity){
+  struct CacheItem {
+    CacheItem(int k, int v):key(k),value(v){}
+    int key;
+    int value;
+  };
+  typedef std::list<CacheItem>::iterator ItemPos;
+  struct CachePos {
+    CachePos(int v, const ItemPos & p):value(v),pos(p) {}
+    int value;
+    ItemPos pos;
+  };
+
+  LRUCache(int capacity) : m_capacity(capacity) {
 
   }
 
   int get(int key) {
-    if (m_hashmap.count(key)==0) {
+    if (m_mapper.find(key) == m_mapper.end() ) {
       return -1;
     } else {
-      ListIterator iter = m_hashmap[key];
-      KeyValuePair pair = *iter;
-      m_cache_item_list.erase(iter);
-      m_cache_item_list.push_front(pair);
-      m_hashmap[key] = m_cache_item_list.begin();
-      return pair.second;
+      int value = m_mapper[key].value;
+      touch(key,value);
+      return value;
     }
   }
 
   void set(int key, int value) {
-    if (m_hashmap.count(key)!=0) {
-      m_cache_item_list.erase(m_hashmap[key]);
-    }
-    m_cache_item_list.push_front(KeyValuePair(key,value));
-    m_hashmap[key] = m_cache_item_list.begin();
-    if (m_cache_item_list.size() > m_capacity) {
-      m_hashmap.erase(m_cache_item_list.back().first);
-      m_cache_item_list.pop_back();
-    }
+    touch(key,value);
   }
 private:
+  // delete if key exist in list, and reinsert it at the front of the list.
+  void touch(int key,int value) {
+    if (m_mapper.find(key)!=m_mapper.end()) {
+      m_lru.erase(m_mapper[key].pos);
+    }
+    m_lru.push_front(CacheItem(key,value));
+    m_mapper[key] = CachePos(value,m_lru.begin());
+    if (m_lru.size() == m_capacity+1) {
+      m_mapper.erase(m_lru.back().key);
+      m_lru.pop_back();
+    }
+  }
   int m_capacity;
-  typedef std::pair<int,int> KeyValuePair;
-  std::list<KeyValuePair> m_cache_item_list;
-  typedef std::list<KeyValuePair>::iterator ListIterator;
-  unordered_map<int,ListIterator> m_hashmap; //<key,ListIterator>
+  std::list<CacheItem> m_lru;
+  std::unordered_map<int,CachePos> m_mapper;
 };
