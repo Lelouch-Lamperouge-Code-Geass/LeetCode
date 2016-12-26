aefdf9a74a22f94f55033512ea131e3e03a087bc
Augustus Pope <yl916@nyu.edu>
2016-12-25 22:29:18 -0500

    new file:134_Gas_Station.cpp  new file:141_Linked_List_Cycle.cpp  new file:142_Linked_List_Cycle_II.cpp  new file:147_Insertion_Sort_List.cpp  new file:160_Intersection_of_Two_Linked_Lists.cpp  new file:167_Two_Sum_II.cpp  new file:169_Majority_Element.cpp  new file:179_Largest_Number.cpp  new file:202_Happy_Number.cpp  new file:208_Implement_Trie.cpp

--- 166_Fraction_to_Recurring_Decimal.cpp
+++ 166_Fraction_to_Recurring_Decimal.cpp
@@ -1,27 +1,32 @@
+// Copyright (C) 2016 by Yang Liu
 class Solution {
 public:
   string fractionToDecimal(int numerator, int denominator) {
-    if (denominator == 0) return "INVALID";
+    if (denominator == 0) return "NAN";
     if (numerator == 0) return "0";
-    std::string res("");
-    int sign = (numerator<0 ^ denominator<0) ? -1 : 1;
-    if (sign==-1) res.push_back('-');
-    long abs_num(std::labs(numerator)), abs_deno(std::labs(denominator));
-    res += std::to_string(abs_num/abs_deno);
-    if (abs_num%abs_deno==0) return res;
+    string res("");
+    if (numerator < 0 ^ denominator < 0) res.push_back('-');
+
+    long abs_nume = std::labs(numerator),
+                    abs_deno = std::labs(denominator);
+    // get interger part
+    res += std::to_string(abs_nume / abs_deno);
+
+    abs_nume %= abs_deno;
+    if (abs_nume == 0) return res;
     res.push_back('.');
-    abs_num %= abs_deno;
-    unordered_map<int,std::size_t> visited;
-    while (abs_num) {
-      if (visited.count(abs_num)!=0) {
-        res.insert(res.begin()+visited[abs_num],'(');
+
+    unordered_map<int, int> visited;
+    while (abs_nume) {
+      if (visited.find(abs_nume)!=visited.end()) {
+        res.insert(res.begin() + visited[abs_nume], '(');
         res.push_back(')');
         break;
       } else {
-        visited[abs_num] = res.size();
-        abs_num *= 10;
-        res.push_back('0'+abs_num/abs_deno);
-        abs_num %= abs_deno;
+        visited[abs_nume] = res.size();  // mark the position of res
+        abs_nume *= 10;
+        res.push_back('0' + abs_nume / abs_deno);
+        abs_nume %= abs_deno;
       }
     }
     return res;
