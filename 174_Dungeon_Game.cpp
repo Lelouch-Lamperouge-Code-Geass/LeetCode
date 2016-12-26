36f7558c0ef8a1047705844210b1028e3b863747
Augustus Pope <yl916@nyu.edu>
2016-12-25 22:15:37 -0500

    renamed:    dungeon_game.cpp -> 174_Dungeon_Game.cpp 	deleted:    add_and_search_word.cpp 	deleted:    binary_tree_right_side_view.cpp 	deleted:    bitwise_and_of_numbers_range.cpp 	deleted:    candy.cpp 	deleted:    compare_version_numbers.cpp 	deleted:    copy_list_with_random_pointer.cpp 	deleted:    course_schedule.cpp 	deleted:    course_schedule_ii.cpp 	deleted:    excel_sheet_column_title.cpp 	deleted:    factorial_trailing_zeroes.cpp 	deleted:    find_minimum_in_rotated_sorted_array.cpp 	deleted:    find_minimum_in_rotated_sorted_array_ii.cpp 	deleted:    find_peak_element.cpp         new: 135_Candy.cpp         new: 138_Copy_List_with_Random_Pointer.cpp         new: 153_Find_Minimum_in_Rotated_Sorted_Array.cpp         new: 154_Find_Minimum_in_Rotated_Sorted_Array_II.cpp         new: 162_Find_Peak_Element.cpp         new: 165_Compare_Version_Numbers.cpp         new: 168_Excel_Sheet_Column_Title.cpp         new: 172_Factorial_Trailing_Zeroes.cpp         new: 199_Binary_Tree_Right_Side_View.cpp         new: 201_Bitwise_AND_of_Numbers_Range.cpp         new: 207_Course_Schedule.cpp         new: 210_Course_Schedule_II.cpp         new: 211_Add_and_Search_Word.cpp

--- 174_Dungeon_Game.cpp
+++ 174_Dungeon_Game.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
   int calculateMinimumHP(vector<vector<int>>& dungeon) {
     if (dungeon.empty()) return 1;
-    const std::size_t row_size(dungeon.size()),col_size(dungeon[0].size());
-    // record the minimal hp when enter the room
-    std::vector< std::vector<int> > min_hp(row_size+1,std::vector<int>(col_size+1,INT_MAX));
-    min_hp[row_size-1][col_size] =  min_hp[row_size][col_size-1] = 1;
-    // since we use size_t here, need hanlde 0-1 scenario
-    for (std::size_t i = row_size-1;i>=0&&i<row_size;--i) {
-      for (std::size_t j = col_size-1;j>=0&&j<col_size;--j) {
-        min_hp[i][j] = std::max(1,std::min(min_hp[i+1][j],min_hp[i][j+1]) - dungeon[i][j]);
+    const int m(dungeon.size()), n(dungeon[0].size());
+    vector< vector<int> > min_hp(m+1,vector<int>(n+1,INT_MAX));
+    min_hp[m][n-1] = min_hp[m-1][n] = 1;
+    for (int i=m-1;i>=0;--i) {
+      for (int j=n-1;j>=0;--j) {
+        min_hp[i][j] = std::min(min_hp[i+1][j],min_hp[i][j+1]) - dungeon[i][j];
+        min_hp[i][j] = std::max(1,min_hp[i][j]);
       }
     }
     return min_hp[0][0];
