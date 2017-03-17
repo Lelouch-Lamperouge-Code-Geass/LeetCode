dc5dd439dc859a9779e98c8f1e629b3ce3718d2b
Augustus Pope <yl916@nyu.edu>
2017-03-17 19:54:00 -0400

    remove files

--- 141_Linked_List_Cycle.cpp
+++
@@ -1,9 +0,0 @@
-bool hasCycle(ListNode *head) {
-  ListNode *slow(head),*fast(head);
-  while (fast && fast->next) {
-    fast = fast->next->next;
-    slow = slow->next;
-    if (slow == fast) return true;
-  }
-  return false;
-}
--- 258_Add_Digits.cpp
+++
@@ -1,6 +0,0 @@
-class Solution {
-public:
-  int addDigits(int num) {
-    return num<=9 ?  num : num % 9 ;
-  }
-};
