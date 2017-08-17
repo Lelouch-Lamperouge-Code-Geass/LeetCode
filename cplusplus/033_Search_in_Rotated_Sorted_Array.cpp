86b3cd78f0b7259092aa8e80ec73568b7b0103ad
Yang Liu <yangliu1989@nyu.edu>
2017-08-16 21:25:44 -0400

    Update 033_Search_in_Rotated_Sorted_Array.cpp

--- cplusplus/033_Search_in_Rotated_Sorted_Array.cpp
+++ cplusplus/033_Search_in_Rotated_Sorted_Array.cpp
@@ -1,10 +1,20 @@
-/*
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+  
+# Solution
+  
 Notice that nums[mid] can have two status, each status divide the 
 vectory into two parts.
 (1) nums[mid] < nums[high], then [mid,high] is increasing
 (2) nums[mid] > nums[high], then [low,mid] is increasing
 We can use binary search based on the status.
-*/
+
+```cpp  
 class Solution {
 public:
   int search(vector<int>& nums, int target) {
@@ -29,3 +39,42 @@ public:
     return -1;
   }
 };
+```
+
+If the input vector's size is quite big, we need to use std::size_t.
+  
+```cpp
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if (nums.empty()) return -1;
+        
+        std::size_t low(0), high(nums.size() - 1);
+        
+        while (low <= high) {
+            std::size_t mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else {
+                if (nums[high] < nums[mid]) { // [low,mid] monotonically increasing
+                    if (target >= nums[low] && target < nums[mid]) {
+                        if (mid == 0) break;
+                        high = mid - 1;
+                    } else {
+                        low = mid + 1;
+                    }
+                } else { // nums[high] >= nums[mid], [mid,high] monotonically increasing
+                    if (target > nums[mid] && target <= nums[high]) {
+                        low = mid + 1;
+                    } else {
+                        if (mid == 0) break;
+                        high = mid - 1;
+                    }
+                }
+            }
+        }
+        
+        return -1;
+    }
+};
+```
