6bc405bf35947e33e419e3daf95aa1d1b1a373d3
Yang Liu <yangliu1989@nyu.edu>
2017-12-21 12:01:26 -0500

    Update 172_Factorial_Trailing_Zeroes.cpp

--- cplusplus/172_Factorial_Trailing_Zeroes.cpp
+++ cplusplus/172_Factorial_Trailing_Zeroes.cpp
@@ -1,3 +1,19 @@
+Given an integer n, return the number of trailing zeroes in n!.
+
+Note: Your solution should be in logarithmic time complexity.
+  
+  
+# Solution
+  
+The idea is:
+
+1. The ZERO comes from 10.
+2. The 10 comes from 2 x 5
+3. And we need to account for all the products of 5 and 2. likes 4×5 = 20 ...
+4. So, if we take all the numbers with 5 as a factor, we'll have way more than enough even numbers to pair with them to get factors of 10  
+  
+
+```cpp
 class Solution {
 public:
   int trailingZeroes(int n) {
@@ -9,3 +25,4 @@ public:
     return result;
   }
 };
+```
